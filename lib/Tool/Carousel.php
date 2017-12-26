1f0c36ea32413e47b853818f30f4cd764c22b1a4
sinharakesh <rksinha.btech@gmail.com>
2017-12-26 10:58:32 +0530

    tool carousel for home slide are added

--- lib/Tool/Carousel.php
+++ lib/Tool/Carousel.php
@@ -15,6 +15,10 @@ class Tool_Carousel extends \xepan\cms\View_Tool{
 			return;
 		}
 		
+
+		$this->slide_effect = ['fadetotopfadefrombottom','zoomout','zoomin'];
+
+
 		$image_m = $this->add('xepan\cms\Model_CarouselImage');
 		$image_m->addCondition([['carousel_category_id',$this->options['carousel_category']],['carousel_category',$this->options['carousel_category']]]);
 		$image_m->setOrder('order','asc');
@@ -26,24 +30,16 @@ class Tool_Carousel extends \xepan\cms\View_Tool{
 		$this->app->jquery->addStaticInclude('revolution/js/jquery.themepunch.revolution.min');
 		$this->app->jquery->addStaticInclude('owl.carousel.min');
 
-		// $temp = $this->add('GiTemplate');
-		// $temp->loadTemplateFromString('<li data-target="#'.$carousel_cl->name.'" data-slide-to="{$count}" class="{$activeindicator}"></li>');
-		// $carousel_indicator = $carousel_cl->add('Lister',null,'carousel_indicator',$temp);
-		// $carousel_indicator->setModel($image_m);
-
+		$this->slide_count = 0;
 		$carousel_cl->addHook('formatRow',function($l){
 			$l->current_row['file'] = './websites/'.$this->app->current_website_name."/".$l->model['file_id'];
+			$l->current_row_html['description']	 = $l->model['text_to_display'];
+			$l->current_row_html['slide_transition'] = $this->slide_effect[$this->slide_count];
+			
+			$this->slide_count++;
+			if($this->slide_count > count($this->slide_effect))
+				$this->slide_count = 0;
 		});
 
-		// $carousel_indicator->addHook('formatRow',function($l){
-		// 	if($this->active_indicator_count == 1)
-		// 		$l->current_row_html['activeindicator'] = "active";
-		// 	else
-		// 		$l->current_row_html['activeindicator'] = "deactive";
-		// 	$this->active_indicator_count++;
-
-		// 	$l->current_row_html['count'] = $this->indicator_count;
-		// 	$this->indicator_count ++;
-		// });
 	}
 }
