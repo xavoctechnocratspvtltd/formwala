40c746961b55164b1cdd1ee3f44e2ed477fcf505
sinharakesh <rksinha.btech@gmail.com>
2017-12-26 11:41:40 +0530

    carousel with slide show testimonial are added

--- lib/Tool/Carousel.php
+++ lib/Tool/Carousel.php
@@ -3,7 +3,10 @@
 namespace xavoc\formwala;
 
 class Tool_Carousel extends \xepan\cms\View_Tool{
-	public $options = [];
+	public $options = [
+			'display_type'=>'slideshow',
+
+		];
 
 	function init(){
 		parent::init();
@@ -14,17 +17,29 @@ class Tool_Carousel extends \xepan\cms\View_Tool{
 			$this->add('View')->set('Please Select Carousel Category');
 			return;
 		}
+		$this->image_model = $this->add('xepan\cms\Model_CarouselImage');
+		$this->image_model->addCondition([['carousel_category_id',$this->options['carousel_category']],['carousel_category',$this->options['carousel_category']]]);
+		$this->image_model->setOrder('order','asc');
 		
+		switch ($this->options['display_type']) {
+				case 'slideshow':
+					$this->slideShow();
+					break;
+				case 'brandCarousel':
+					$this->brandCarousel();
+					break;
+				case 'testimonial':
+					$this->testimonial();
+					break;
+			}
+	}
+
+	function slideShow(){
 
 		$this->slide_effect = ['fadetotopfadefrombottom','zoomout','zoomin'];
 
-
-		$image_m = $this->add('xepan\cms\Model_CarouselImage');
-		$image_m->addCondition([['carousel_category_id',$this->options['carousel_category']],['carousel_category',$this->options['carousel_category']]]);
-		$image_m->setOrder('order','asc');
-
 		$carousel_cl = $this->add('CompleteLister',null,null,['view\tool\carousel']);
-		$carousel_cl->setModel($image_m);
+		$carousel_cl->setModel($this->image_model);
 		
 		$this->app->jquery->addStaticInclude('revolution/js/jquery.themepunch.tools.min');
 		$this->app->jquery->addStaticInclude('revolution/js/jquery.themepunch.revolution.min');
@@ -40,6 +55,25 @@ class Tool_Carousel extends \xepan\cms\View_Tool{
 			if($this->slide_count > count($this->slide_effect))
 				$this->slide_count = 0;
 		});
+	}
 
+	function brandCarousel(){
+		$carousel_cl = $this->add('CompleteLister',null,null,['view\tool\carouselmulti']);
+		$carousel_cl->setModel($this->image_model);
+
+		$carousel_cl->addHook('formatRow',function($l){
+			$l->current_row['file'] = './websites/'.$this->app->current_website_name."/".$l->model['file_id'];
+			$l->current_row_html['description']	 = $l->model['text_to_display'];
+		});
+	}
+
+	function testimonial(){
+		$carousel_cl = $this->add('CompleteLister',null,null,['view\tool\carouseltestimonial']);
+		$carousel_cl->setModel($this->image_model);
+
+		$carousel_cl->addHook('formatRow',function($l){
+			$l->current_row['file'] = './websites/'.$this->app->current_website_name."/".$l->model['file_id'];
+			$l->current_row_html['description']	 = $l->model['text_to_display'];
+		});
 	}
 }
