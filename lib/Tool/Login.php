f3cc16a85849ca4ba4b66f2d3ba9c59691b77e29
sinharakesh <rksinha.btech@gmail.com>
2017-12-27 15:33:58 +0530

    login resend implemented

--- lib/Tool/Login.php
+++ lib/Tool/Login.php
@@ -19,12 +19,12 @@ class Tool_Login extends \xepan\cms\View_Tool{
 		if(!$is_otp_send){
 			$form = $this->add('Form');
 			$form->addField('Number','mobile_no');
-			$form->addSubmit('Send OTP');
+			$form->addSubmit('Send OTP')->addClass('btn btn-info');
+
 			if($form->isSubmitted()){
 
 				$otp = rand(1111,9999);
 				// base user model entry 
-
 				$user = $this->add('xepan\base\Model_User');
 				$this->add('BasicAuth')
 				->usePasswordEncryption('md5')
@@ -52,7 +52,7 @@ class Tool_Login extends \xepan\cms\View_Tool{
 					$temp->loadTemplateFromString($message);
 					$msg = $this->add('View',null,null,$temp);
 					$msg->template->trySetHTML('otp',$otp);
-					
+															
 					$this->add('xepan\communication\Controller_Sms')->sendMessage($form['mobile_no'],$msg->getHtml());
 				}
 				$this->js(null)->reload(['otp_send'=>true,'uid'=>$form['mobile_no'],'course'=>$course])->execute();
@@ -63,29 +63,72 @@ class Tool_Login extends \xepan\cms\View_Tool{
 				return;
 			}
 
+			$this->add('View')->addClass('alert alert-success')->setHtml('<strong style="font-size:16px;"> OTP has been sent to your mobile number ('.$mobile_no.') </strong>');
 			$form = $this->add('Form');
-			// $form->addField('Number','mobile_no');
-			$form->addField('Number','otp');
-			$form->addSubmit('Login');
-			$auth=$this->app->auth;
+			$form->addField('line','mobile_no')->set($mobile_no)->setAttr('disabled',true);
+			$form->addField('line','otp');
+			$submit_btn = $form->addSubmit('Submit')->addClass('btn btn-primary');
+			$resend_btn = $form->addSubmit('Resend OTP')->addClass('btn btn-warning')->setStyle('margin-left','10px;');
+
+			$auth = $this->app->auth;
 
 			if($form->isSubmitted()){
+				if($form->isClicked($resend_btn)){
+					
+					$otp = rand(1111,9999);
+					// base user model entry 
+					$user = $this->add('xepan\base\Model_User');
+					$this->add('BasicAuth')
+						->usePasswordEncryption('md5')
+						->addEncryptionHook($user);
+				
+					$user->addCondition('username',$mobile_no);
+					$user->addCondition('scope','WebsiteUser');
+					$user->tryLoadAny();
+					$user['password'] = $otp;
+					$user['type'] = 'User';
+					$user['hash'] = $otp;
+					$user['status'] = "Active";
+					$user->save();
 
-				// login user and redirect to apllication form
-				if(!$credential = $this->app->auth->verifyCredentials($mobile_no,$form['otp'])){
-					$form->displayError($form->getElement('otp'),'wrong credentials');
-				}
+					$applicant = $this->add('xavoc\formwala\Model_Applicant');
+					$applicant->addCondition('mobile_no',$mobile_no);
+					$applicant->addCondition('user_id',$user->id);
+					$applicant->tryLoadAny();
+					$applicant->save();
 
-				$user = $this->add('xepan\base\Model_User')->load($credential);
-				if($user['status']=='Inactive')
+					$message = $this->app->getConfig('otpMessage','{$otp}, this is Your OTP.');
+				
+					if($message){
+						$temp = $this->add('GiTemplate');
+						$temp->loadTemplateFromString($message);
+						$msg = $this->add('View',null,null,$temp);
+						$msg->template->trySetHTML('otp',$otp);
+					
+						$this->add('xepan\communication\Controller_Sms')->sendMessage($mobile_no,$msg->getHtml());
+					}
+					$this->js(null)->reload(['otp_send'=>true,'uid'=>$mobile_no,'course'=>$course])->execute();
 
-				$form->displayError('otp','Please Activate Your Account First');
-				$auth->login($mobile_no);
+				}else{
+					if(!is_numeric($form['otp']))
+						throw $this->exception('otp must be number','ValidityCheck')->setField('otp');
 
-				$this->app->stickyForget('otp_send');
-				$this->app->stickyForget('uid');
+					// login user and redirect to apllication form
+					if(!$credential = $this->app->auth->verifyCredentials($mobile_no,$form['otp'])){
+						$form->displayError($form->getElement('otp'),'wrong credentials');
+					}
 
-				$this->app->redirect($this->app->url($this->options['account_page'],['course'=>$course]))->execute();
+					$user = $this->add('xepan\base\Model_User')->load($credential);
+					if($user['status']=='Inactive')
+
+					$form->displayError('otp','Please Activate Your Account First');
+					$auth->login($mobile_no);
+
+					$this->app->stickyForget('otp_send');
+					$this->app->stickyForget('uid');
+
+					$this->app->redirect($this->app->url($this->options['account_page'],['course'=>$course]))->execute();
+				}				
 			}
 		}
 	}
