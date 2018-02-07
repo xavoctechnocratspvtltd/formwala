f7f55f65b69ed9eca9ddfc98eeac1d85cd232b68
sinharakesh <rksinha.btech@gmail.com>
2018-02-07 12:53:46 +0530

    carousel mouse hover auto stop and leave auto start added

--- page/test.php
+++ page/test.php
@@ -7,7 +7,9 @@ class page_test extends \xepan\base\Page{
 	function init(){
 		parent::init();
 
-		$o = $this->add('GiTemplate')->loadTemplate('view/fileupload')->set('url',$this->app->url())->render();
+		// $m = $this->add('xepan\filestore\Model_Image')->load(8345);
+		// $m->delete();
+		// $o = $this->add('GiTemplate')->loadTemplate('view/fileupload')->set('url',$this->app->url())->render();
 		// echo htmlentities($o);
 	}	
 }
